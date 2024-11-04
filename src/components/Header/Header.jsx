e676ec000332ee64063496fcbff4ffc6c6d9cd36
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-11-04 22:20:13 +0600

    useNavigate used

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,12 +8,10 @@ const Header = () => {
       <nav>
         <span>My Website</span>
         <Link to="/">Home</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/posts">Posts</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact Us</Link>
-
-        {/* <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact Us</a> */}
       </nav>
     </div>
   );
---
+++ src/components/User/User.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const User = ({ user }) => {
+  const { id, name, email, phone } = user;
+  const userStyle = {
+    border: "2px solid yellow",
+    padding: "5px",
+    borderRadius: "10px",
+    marginBottom: "40px",
+  };
+
+  return (
+    <div style={userStyle}>
+      <h1>{name}</h1>
+      <p>Email: {email}</p>
+      <p>Phone: {phone}</p>
+      <Link to={`/user/${id}`}>Show Details</Link>
+      <Link to={`/user/${id}`}>
+        <button>Click Me</button>
+      </Link>
+    </div>
+  );
+};
+
+User.propTypes = {
+  user: PropTypes.object,
+};
+
+export default User;
